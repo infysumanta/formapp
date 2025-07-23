69ffc7ab20dff91f51fc05e3bd1a0c4f42f5213e
Sumanta <sumantablog@gmail.com>
2025-07-23 15:01:15 +0530

    feat(app): implement form loading and submission handling
    - Added data attributes for form integration.
    - Included script for form loader.
    - Implemented event listeners for form lifecycle events.
    - Added validation to prevent submission unless first name is 'John'.

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,42 @@
 <template>
-  <div>Hi</div>
+  <div
+    data-form-id="76438228-4767-f011-bec2-00224809e97b"
+    data-form-api-url="https://public-usa.mkt.dynamics.com/api/v1.0/orgs/14e74438-32ad-42c2-91eb-3b40da498c56/landingpageforms"
+    data-cached-form-url="https://assets-usa.mkt.dynamics.com/14e74438-32ad-42c2-91eb-3b40da498c56/digitalassets/forms/76438228-4767-f011-bec2-00224809e97b"
+  ></div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/usa/FormLoader/FormLoader.bundle.js",
+      defer: true,
+    },
+  ],
+});
+</script>
+<script>
+document.addEventListener("d365mkt-beforeformload", function () {
+  console.log("d365mkt-beforeformload");
+});
+document.addEventListener("d365mkt-afterformload", function () {
+  console.log("d365mkt-afterformload");
+});
+document.addEventListener("d365mkt-formrender", function () {
+  console.log("d365mkt-formrender");
+});
+document.addEventListener("d365mkt-formsubmit", function (event) {
+  // Example of validation using form submit event - cancelling form submission unless first name is John
+  if (document.forms[0]["firstname"].value !== "John") {
+    event.preventDefault();
+    console.log("blocked mkt-formsubmit");
+    return;
+  }
+  console.log("mkt-formsubmit" + JSON.stringify(event.detail.payload));
+});
+document.addEventListener("d365mkt-afterformsubmit", function (event) {
+  console.log("success - " + event.detail.successful);
+  console.log("payload - " + JSON.stringify(event.detail.payload));
+});
+</script>
